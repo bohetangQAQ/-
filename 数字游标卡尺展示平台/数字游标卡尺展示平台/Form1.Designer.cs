5492d33b603f51f2449ea199dcf24824880cbf80
bohetangQAQ <59499174+bohetangQAQ@users.noreply.github.com>
2021-12-07 15:49:27 +0800

    Add files via upload

---
+++ 数字游标卡尺展示平台/数字游标卡尺展示平台/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿
+namespace 数字游标卡尺展示平台
+{
+    partial class 数字游标卡尺展示平台
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mm = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inch = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.hslGauge1 = new HslControls.HslGauge();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(11, 88);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "数据：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(11, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "串口：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(90, 12);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(106, 25);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(205, 7);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "打开串口";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(78, 76);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 46);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "数据1";
+            // 
+            // mm
+            // 
+            this.mm.AutoSize = true;
+            this.mm.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.mm.Location = new System.Drawing.Point(202, 76);
+            this.mm.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.mm.Name = "mm";
+            this.mm.Size = new System.Drawing.Size(175, 46);
+            this.mm.TabIndex = 7;
+            this.mm.Text = "mm/毫米";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(78, 149);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 46);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "数据2";
+            // 
+            // inch
+            // 
+            this.inch.AutoSize = true;
+            this.inch.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.inch.Location = new System.Drawing.Point(202, 149);
+            this.inch.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.inch.Name = "inch";
+            this.inch.Size = new System.Drawing.Size(170, 46);
+            this.inch.TabIndex = 9;
+            this.inch.Text = "in   /英寸";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label5.Location = new System.Drawing.Point(76, 505);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(242, 42);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "数据保存成功！";
+            this.label5.Visible = false;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(411, 7);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(376, 542);
+            this.listView1.TabIndex = 16;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(309, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 37);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "清除数据";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // hslGauge1
+            // 
+            this.hslGauge1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.hslGauge1.Location = new System.Drawing.Point(21, 216);
+            this.hslGauge1.Name = "hslGauge1";
+            this.hslGauge1.Size = new System.Drawing.Size(356, 150);
+            this.hslGauge1.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(83, 409);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 29);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(210, 410);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 29);
+            this.textBox2.TabIndex = 20;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(16, 369);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 28);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "设置串口：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(139, 413);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 22);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "mm  -";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(83, 465);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 37);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "设置";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(210, 465);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 37);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "清除";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.Location = new System.Drawing.Point(276, 416);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 22);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "mm";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // 数字游标卡尺展示平台
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 556);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.hslGauge1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.inch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "数字游标卡尺展示平台";
+            this.Text = "数字游标卡尺展示平台";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label mm;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label inch;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private HslControls.HslGauge hslGauge1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ 数字游标卡尺展示平台/数字游标卡尺展示平台/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace 数字游标卡尺展示平台
+{
+    public partial class 数字游标卡尺展示平台 : Form
+    {
+        double[] shu1 = new double[1000];
+        double[] shu2 = new double[1000];
+        //用于标识数据序号
+        int i = 0;
+        //串口
+        int j = 0;
+        int k = 0;
+        //声明一个com对象
+        public SerialPort COM;
+        public 数字游标卡尺展示平台()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string[] coms = SerialPort.GetPortNames();
+            foreach (string com in coms)
+            {
+                comboBox1.Items.Add(com);
+            }
+            //定义控件表头
+            listView1.View = View.Details;
+            ColumnHeader header1 = new ColumnHeader();
+            header1.Text = "数据";
+            header1.TextAlign = HorizontalAlignment.Center;
+            header1.Width = 40;
+            listView1.Columns.Add(header1);
+
+            ColumnHeader header2 = new ColumnHeader();
+            header2.Text = "mm/毫米";
+            header2.TextAlign = HorizontalAlignment.Center;
+            header2.Width = 80;
+            listView1.Columns.Add(header2);
+
+            ColumnHeader header3 = new ColumnHeader();
+            header3.Text = "in/英寸";
+            header3.TextAlign = HorizontalAlignment.Center;
+            header3.Width = 80;
+            listView1.Columns.Add(header3);
+
+            ColumnHeader header4 = new ColumnHeader();
+            header4.Text = "记录时间";
+            header4.TextAlign = HorizontalAlignment.Center;
+            header4.Width = 170;
+            listView1.Columns.Add(header4);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int g = 0;
+            if (COM == null & g == 0) 
+            {
+                //创建一个串口
+                COM = new SerialPort(comboBox1.Text, 115200);
+
+                COM.DataReceived += COM_DataReceived;
+
+                COM.Open();
+
+                button1.Text = "关闭串口";
+
+                g = 1;
+            }
+            else if(g == 1)
+            {
+                button1.Text = "打开串口";
+                g = 0;
+            }
+        }
+
+        private void COM_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            System.Threading.Thread.Sleep(10);
+
+            int len = COM.BytesToRead;
+            if (len>1)
+            {
+                byte[] buff = new byte[len];
+
+                COM.Read(buff,0,len);
+
+
+                //把收到的内容转换成字符串
+                string content = ASCIIEncoding.ASCII.GetString(buff);
+
+                Console.WriteLine(content);
+                
+
+
+                this.Invoke(new Action(
+                    () => {
+                        double f1 = Convert.ToSingle(content);
+
+                        if (f1!=500.00)
+                        {
+                            //数据写入label控件
+                            label3.Text = content;
+
+                            double f = 2.54;
+
+                            double f2 = f1 / f;
+
+                            label4.Text = f2.ToString("0.00");
+
+                            //数据写入图像控件
+                            hslGauge1.Value = Convert.ToSingle(content);
+                        }
+                        else
+                        {
+                            i++;
+
+                            double shu1 = Convert.ToSingle(content);
+                            //double shu2 = f2;
+
+                            //将数据写入控件
+                            listView1.BeginUpdate();
+                            ListViewItem lvi = new ListViewItem();
+                            lvi.Text = "" + i;
+                            lvi.SubItems.Add(label3.Text);
+                            lvi.SubItems.Add(label4.Text);
+                            lvi.SubItems.Add(DateTime.Now.ToLocalTime().ToString());
+                            listView1.Items.Add(lvi);
+                            listView1.EndUpdate();
+
+                            label5.Visible = true;
+                            // MessageBox.Show("数据保存成功");
+                            timer1.Start();
+                        }
+                        //下面代码为备份
+                        /*
+                        else
+                        {
+
+                            label5.Visible = true;
+                            // MessageBox.Show("数据保存成功");
+                            timer1.Start();
+
+                            i++;
+
+                            //将数据写入控件
+                            listView1.BeginUpdate();
+                            ListViewItem lvi = new ListViewItem();
+                            lvi.Text = "" + i;
+                            lvi.SubItems.Add(shu1.ToString("0.00"));
+                            lvi.SubItems.Add(shu2.ToString("0.00"));
+                            lvi.SubItems.Add(DateTime.Now.ToLocalTime().ToString());
+                            listView1.Items.Add(lvi);
+                            listView1.EndUpdate();
+                            //数据写入图像控件
+                            hslGauge1.Value = (int)shu1;
+
+
+                        ===================================================
+                            label3.Text = content;
+
+                            //double f1 = Convert.ToSingle(content);
+
+                            double f = 2.54;
+
+                            double f2 = f1 / f;
+
+                            label4.Text = f2.ToString("0.00");
+
+                            double shu1 = Convert.ToSingle(content);
+                            double shu2 = f2;
+                        }
+                        */
+                    }
+                    ));
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label5.Visible = false;
+            timer1.Stop();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+            DialogResult dh = MessageBox.Show("你确定要清除数据吗","清除数据成功！",MessageBoxButtons.OKCancel,MessageBoxIcon.Exclamation);
+            if(dh == DialogResult.OK)
+            {
+                listView1.Items.Clear();
+                Array.Clear(shu1, 0, shu1.Length);
+                Array.Clear(shu2, 0, shu2.Length);
+                label3.Text = "";
+                label4.Text = "";
+            }
+        }
+
+        private void hslCurveHistory1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            int j = Convert.ToInt32(textBox1.Text);
+            int k = Convert.ToInt32(textBox2.Text);
+            COM.WriteLine(""+j+k);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            COM.WriteLine("0090");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
